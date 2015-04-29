4444f181b265322005b2b0f648fbfdfc862eb0ce
Matt Finucane <matfin@gmail.com>
2015-04-29 10:34:33 +0200

    Fix for firing mouse down event when the slider is not being moved.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'matfin:slider',
-    version: '1.1.0',
+    version: '1.1.1',
     summary: 'Small and lightweight responsive slider library.',
     git: 'https://github.com/matfin/slider.git',
     documentation: 'README.md'
