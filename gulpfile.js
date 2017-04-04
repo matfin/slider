25998b672e80c2b708e25ea0f4d7d13d72045f9d
Matt Finucane <matfin@gmail.com>
2017-04-04 13:28:58 +0200

    Rolling back on minification since there are no compression tools that can handle ES6/7 syntax.

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const 	gulp	= require('gulp'),
-		uglify	= require('gulp-uglify');
-
-gulp.task('default', () => {
-	return gulp
-	.src('_src/slider.js')
-	.uglify()
-	.pipe(gulp.dest('dist/slider.js'));
-});
